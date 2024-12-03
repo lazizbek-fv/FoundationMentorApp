829ee888359c3500bedae64fd54b49aa95038c31
Lazizbek <mywave.17@gmail.com>
2024-12-03 14:19:10 +0100

    Initial Commit

---
+++ FoundationMentorApp/FoundationMentorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationMentorAppApp.swift
+//  FoundationMentorApp
+//
+//  Created by Lazizbek Fayzullayev on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FoundationMentorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
